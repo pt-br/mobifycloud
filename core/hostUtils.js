d62bf6f4e6a69610809ccd898d97a3e6ce387ae2
pt-br <lucas.paixao@objectedge.com>
2017-08-09 02:11:07 -0300

    Use arrow functions on hostUtils

--- core/hostUtils.js
+++ core/hostUtils.js
@@ -15,49 +15,49 @@ let hostPath = '';
 let hostVar = '';
 let environment = '';
 
-d.on('error', function(err) {
+d.on('error', err => {
   console.error(err);
 });
 
 const hostUtils = {
-  updateHostFile: function() {
-    fs.readFile('/etc/hosts', 'utf8', function (err, hostsContent) {
+  updateHostFile: () => {
+    fs.readFile('/etc/hosts', 'utf8', (err, hostsContent) => {
       if (err) {
         return console.log(err);
       }
 
-      const originalHosts = hostsContent;
+      originalHosts = hostsContent;
 
-      let newHostsContent = hostsContent + '\n# MobifyCloud Development Hosts:';
+      let newHostsContent = `${hostsContent} # MobifyCloud Development Hosts:`;
       for (let i = 0; i < routesHost.length; i++) {
         newHostsContent = newHostsContent + '\n' + '127.0.0.1' + '\t' + routesHost[i];
       }
 
       /* Verify if writeFile is successful to avoid Heroku issues */
-      d.run(function() {
-        fs.writeFile('/etc/hosts', newHostsContent, function (err) {
+      d.run(() => {
+        fs.writeFile('/etc/hosts', newHostsContent, err => {
           if (err) throw err;
         });
       });
     });
   },
-  getRoutes: function() {
+  getRoutes: () => {
     let urlRoutesLength = JSON.stringify(url_map.route.length);
     for (let i = 0; i < urlRoutesLength; i++) {
       routes.push(JSON.stringify(url_map.route[i]));
     }
   },
-  cleanupHosts: function() {
+  cleanupHosts: () => {
     console.log('Restoring original /etc/hosts file...');
     fs.writeFileSync('/etc/hosts', originalHosts);
   },
-  getRoutesEndpoint: function() {
+  getRoutesEndpoint: () => {
     for (let i = 0; i < routes.length; i++) {
       routesEndpoint.push(routes[i].split('=> ')[1].replace('\"', ''));
       return routesEndpoint;
     }
   },
-  getRoutesHost: function() {
+  getRoutesHost: () => {
     for (let i = 0; i < routes.length; i++) {
       routesHost.push(routes[i].split('=> ')[0].replace('\"', ''));
       return routesHost[0];
